c2f4d7233e154e85255d701a2b9e0663664b5276
Minqi824 <jiangmq95@163.com>
2025-03-23 12:50:34 +0800

    update scripts

---
+++ scripts/exp_long_term_forecasting_gym_non_transformer_ETT.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
+
+# 指定目录路径
+dir_path="./scripts/long_term_forecast/ETT_script/gym"
+echo "Current directory: $(pwd)"
+
+# 找到所有 .sh 脚本并使用 xargs 并行执行，限制为12个并行进程
+# find "$dir_path" -name '*.sh' -print0 | xargs -0 -n 1 -P 10 bash
+find "$dir_path" -name '*.sh' ! -name '*Transformer*.sh' -print0| shuf -z | xargs -0 -n 1 -P 3 bash
+
+echo "All scripts have been started."
